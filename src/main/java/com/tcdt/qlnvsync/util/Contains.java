61e2fa8715736207a3594bdec083b15a857fb4ba
bahoang84 <bahoang84@gmail.com>
2022-01-12 15:11:39 +0700

    Update

--- src/main/java/com/tcdt/qlnvsync/util/Contains.java
+++ src/main/java/com/tcdt/qlnvsync/util/Contains.java
@@ -1,10 +1,13 @@
 package com.tcdt.qlnvsync.util;
 
 public class Contains {
+	public static final String FORMAT_DATE_STR = "dd/MM/yyyy";
+	public static final String FORMAT_DATE_TIME_STR = "dd/MM/yyyy HH:mm:ss";
+
 	// Trang thai response
 	public static final int RESP_SUCC = 0;
 	public static final int RESP_FAIL = 1;
-	
+
 	public static final String TYPE_USER_BACKEND = "BE";
 	public static final String TYPE_USER_FRONTEND = "FE";
 }
